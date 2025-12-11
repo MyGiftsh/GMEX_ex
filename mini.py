974ac92ab51b7f1ffd2f786aa148bd52e4b271b2
MyGiftsh <sani.04.04@mail.ru>
2025-12-11 04:26:19 +0300

    Update mini.py

--- mini.py
+++ mini.py
@@ -9,7 +9,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 # --- Конфигурация ---
 # Лучше хранить токен в env var: export GMEX_TOKEN="..."
 TOKEN = os.environ.get("GMEX_TOKEN") or "7937830515:AAGTesyU-PbqxswE1_d0g87-hk33XqQmxOU"  # <- Если вы предоставили токен, он здесь
-WEB_APP_URL = os.environ.get("GMEX_WEBAPP_URL") or "https://mygiftsh.github.io/GMEX_ex/"  # обязательно HTTPS
+WEB_APP_URL = os.environ.get("GMEX_WEBAPP_URL") or "https://mygiftsh.github.io/GMEX_ex/?v=5"  # обязательно HTTPS
 ADMIN = os.environ.get("GMEX_ADMIN") or "@gadzhigg"  # может быть "@username" или строка с числовым id
 
 BOT_NAME = "gmex"
@@ -104,4 +104,5 @@ def main():
     app.run_polling()
 
 if __name__ == "__main__":
+
     main()
